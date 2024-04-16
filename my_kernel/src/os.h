34b3cab9a72b9baaa3a8d237bf81b95f1698c8b5
JennyMarip <104782471+JennyMarip@users.noreply.github.com>
2024-04-17 01:39:18 +0800

    实现shell

--- my_kernel/src/os.h
+++ my_kernel/src/os.h
@@ -4,9 +4,9 @@
 // Memory Layout
 #define KERNEL_BASE_ADD 0x80200000L
 // STRING
-#define _PROMPT         0x80201270L
+#define _PROMPT         0x80201280L
 
 // BUFFER
-#define SHELL_BUFFER    0x80201280L
+#define SHELL_BUFFER    0x80201290L
 
 #endif
