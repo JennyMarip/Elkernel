b62aed044643298d3e4ee3f34d1565a0aedcf7e1
JennyMarip <104782471+JennyMarip@users.noreply.github.com>
2024-04-17 11:18:50 +0800

    实现加载应用程序

--- my_kernel/src/os.h
+++ my_kernel/src/os.h
@@ -4,9 +4,14 @@
 // Memory Layout
 #define KERNEL_BASE_ADD 0x80200000L
 // STRING
-#define _PROMPT         0x80201280L
+#define _PROMPT         0x80201260L
 
 // BUFFER
-#define SHELL_BUFFER    0x80201290L
+#define SHELL_BUFFER    0x80201270L
+
+// File System
+// shell
+#define SHELL_FS_ADD    0x80200180L
+#define SHELL_LEN             0x68L
 
 #endif
