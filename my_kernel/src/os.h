f9d85ae3515cbff3797a5a28a14ce6bfc5037a2e
JennyMarip <104782471+JennyMarip@users.noreply.github.com>
2024-04-17 17:38:23 +0800

    将文件系统放在低地址（与bootloader链接），kernel独立

--- my_kernel/src/os.h
+++ my_kernel/src/os.h
@@ -3,15 +3,20 @@
 
 // Memory Layout
 #define KERNEL_BASE_ADD 0x80200000L
+#define SHELL_KSTACK    0x80210000L // 8 KB down 
+#define SHELL_TCB       0x80210000L //      up
+#define APP_BASE_ADD    0x80300000L
+
 // STRING
-#define _PROMPT         0x80201260L
+#define _HELLO_WORLD    0x802011f0L
+#define _PROMPT         0x80201200L
 
 // BUFFER
-#define SHELL_BUFFER    0x80201270L
+#define SHELL_BUFFER    0x80201210L
 
 // File System
 // shell
-#define SHELL_FS_ADD    0x80200180L
+#define SHELL_FS_ADD    0x80000228L
 #define SHELL_LEN             0x68L
 
 #endif
