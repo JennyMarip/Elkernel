147b4371a294bd33d3e86a2490636edca1207c6d
JennyMarip <104782471+JennyMarip@users.noreply.github.com>
2024-04-18 21:38:09 +0800

    shell执行hello程序（one time）

--- my_kernel/src/os.h
+++ my_kernel/src/os.h
@@ -4,8 +4,13 @@
 // Memory Layout
 #define KERNEL_BASE_ADD 0x80200000L
 #define KERNEL_STACK    0x80200000L // 8 KB down
+
 #define SHELL_KSTACK    0x80210000L // 8 KB down 
 #define SHELL_TCB       0x80210000L //      up
+
+#define HELLO_KSTACK    0x80220000L // 8 KB down
+#define HELLO_TCB       0x80220000L //      up
+
 #define APP_BASE_ADD    0x80300000L
 
 // STRING
@@ -19,6 +24,9 @@
 // File System
 // shell
 #define SHELL_FS_ADD    0x80000254L
-#define SHELL_LEN             0x68L
+#define SHELL_LEN             0x5cL
+// hello
+#define HELLO_FS_ADD    0x800002b0L
+#define HELLO_LEN             0x10L
 
 #endif
