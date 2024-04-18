a93b79db01655dfe2f8b54ecece9c2976232d1bd
JennyMarip <104782471+JennyMarip@users.noreply.github.com>
2024-04-18 11:00:12 +0800

    维护TRAP上下文

--- my_kernel/src/os.h
+++ my_kernel/src/os.h
@@ -8,15 +8,15 @@
 #define APP_BASE_ADD    0x80300000L
 
 // STRING
-#define _HELLO_WORLD    0x80001350L
-#define _PROMPT         0x80001360L
+#define _HELLO_WORLD    0x80001380L
+#define _PROMPT         0x80001390L
 
 // BUFFER
-#define SHELL_BUFFER    0x80001370L
+#define SHELL_BUFFER    0x800013a0L
 
 // File System
 // shell
-#define SHELL_FS_ADD    0x80000228L
+#define SHELL_FS_ADD    0x80000250L
 #define SHELL_LEN             0x68L
 
 #endif
