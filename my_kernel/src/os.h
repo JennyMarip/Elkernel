2d71e31dd6f277f64b643187f1bfe2bd3cc1ea96
JennyMarip <104782471+JennyMarip@users.noreply.github.com>
2024-04-18 11:20:21 +0800

    维护CALL上下文

--- my_kernel/src/os.h
+++ my_kernel/src/os.h
@@ -3,20 +3,21 @@
 
 // Memory Layout
 #define KERNEL_BASE_ADD 0x80200000L
+#define KERNEL_STACK    0x80200000L // 8 KB down
 #define SHELL_KSTACK    0x80210000L // 8 KB down 
 #define SHELL_TCB       0x80210000L //      up
 #define APP_BASE_ADD    0x80300000L
 
 // STRING
-#define _HELLO_WORLD    0x80001380L
-#define _PROMPT         0x80001390L
+#define _HELLO_WORLD    0x800013b0L
+#define _PROMPT         0x800013c0L
 
 // BUFFER
-#define SHELL_BUFFER    0x800013a0L
+#define SHELL_BUFFER    0x800013d0L
 
 // File System
 // shell
-#define SHELL_FS_ADD    0x80000250L
+#define SHELL_FS_ADD    0x80000254L
 #define SHELL_LEN             0x68L
 
 #endif
